e4581416c18d0594e715a83735289a85a88f21b1
Rainer Bachleitner <rbachleitner@gmail.com>
2018-07-03 14:48:01 +0200

    Add jest testing framework and first tests

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,34 @@
+import App from '../App';
+import Bubble from '../components/Bubble';
+import { shallowMount, mount } from '@vue/test-utils';
+
+describe('App', () => {
+  it('can be mounted', () => {
+    const wrapper = mount(App);
+  });
+
+  it('has a method onBubbleMouseEnter that sets a zoomedOut flag true for the bubble', () => {
+    const wrapper = mount(App);
+    expect(wrapper.vm.bubbles[0].zoomedOut).toBe(false);
+    wrapper.find('circle').trigger('mouseover');
+    expect(wrapper.vm.bubbles[0].zoomedOut).toBe(true);
+  });
+
+  it('has a method onBubbleMouseLeave that sets a zoomedOut flag false for the bubble', () => {
+    const wrapper = mount(App);
+    wrapper.setData({
+      bubbles : [
+        {
+          id: 1,
+          x: 300,
+          y: 300,
+          r: 74.4123,
+          title: "Cool executive functions, Hot and cool, Cool and hot",
+          zoomedOut: true
+        }
+      ]});
+    expect(wrapper.vm.bubbles[0].zoomedOut).toBe(true);
+    wrapper.find('circle').trigger('mouseout');
+    expect(wrapper.vm.bubbles[0].zoomedOut).toBe(false);
+  });
+});
---
+++ src/__tests__/Chart.test.js
@@ -0,0 +1,22 @@
+import Chart from '../components/Chart';
+import { shallowMount, mount } from '@vue/test-utils';
+
+const factory = (values = {}) => {
+  return shallowMount(Chart, {
+    propsData: { ...values}
+  })
+};
+
+describe('Chart', () => {
+  it('renders an svg chart with a given height and width', () => {
+    const wrapper = factory({height: 600, width: 600});
+    expect(wrapper.contains('svg')).toEqual(true);
+    expect(wrapper.find('svg').attributes().height).toBe("600");
+    expect(wrapper.find('svg').attributes().width).toBe("600");
+  });
+
+  it('initially has class zoomed_out', () => {
+    const wrapper = mount(Chart);
+    expect(wrapper.find('rect').attributes().class).toBe('zoomed_out');
+  });
+});
